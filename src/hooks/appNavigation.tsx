218c2984e810e0ba912884c06fe1e6e6365cc4e0
cjmot <146262493+cjmot@users.noreply.github.com>
2026-02-10 12:15:43 -0700

    prettier reformat

--- src/hooks/appNavigation.tsx
+++ src/hooks/appNavigation.tsx
@@ -1,20 +1,20 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 
 function useBreadcrumb(sibling?: string) {
-  const location = useLocation();
-  const navigate = useNavigate();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  const navigateByBreadcrumb = () => {
-    let newPath = location.pathname.substring(0, location.pathname.lastIndexOf('/'));
-    if (sibling) {
-      newPath = newPath + '/' + sibling;
-    } else if (newPath === '') {
-      newPath = '/';
-    }
-    navigate(newPath, { state: location.state });
-  };
+    const navigateByBreadcrumb = () => {
+        let newPath = location.pathname.substring(0, location.pathname.lastIndexOf('/'));
+        if (sibling) {
+            newPath = newPath + '/' + sibling;
+        } else if (newPath === '') {
+            newPath = '/';
+        }
+        navigate(newPath, { state: location.state });
+    };
 
-  return navigateByBreadcrumb;
+    return navigateByBreadcrumb;
 }
 
 export { useBreadcrumb };
--- src/views/closeStore.tsx
+++ src/views/closeStore.tsx
@@ -6,24 +6,30 @@ import View from './view';
 import Button from '../components/button';
 
 export default function CloseStore() {
-  const state = useLocation().state;
-  const navigateToParent = useBreadcrumb();
+    const state = useLocation().state;
+    const navigateToParent = useBreadcrumb();
 
-  async function close() {
-    await pizzaService.closeStore(state.franchise, state.store);
-    navigateToParent();
-  }
+    async function close() {
+        await pizzaService.closeStore(state.franchise, state.store);
+        navigateToParent();
+    }
 
-  return (
-    <View title='Sorry to see you go'>
-      <div className='text-start py-8 px-4 sm:px-6 lg:px-8'>
-        <div className='text-neutral-100'>
-          Are you sure you want to close the <span className='text-orange-500'>{state.franchise.name}</span> store <span className='text-orange-500'>{state.store.name}</span> ? This cannot be
-          restored. All outstanding revenue will not be refunded.
-        </div>
-        <Button title='Close' onPress={close} />
-        <Button title='Cancel' onPress={navigateToParent} className='bg-transparent border-neutral-300' />
-      </div>
-    </View>
-  );
+    return (
+        <View title="Sorry to see you go">
+            <div className="text-start py-8 px-4 sm:px-6 lg:px-8">
+                <div className="text-neutral-100">
+                    Are you sure you want to close the{' '}
+                    <span className="text-orange-500">{state.franchise.name}</span> store{' '}
+                    <span className="text-orange-500">{state.store.name}</span> ? This cannot be
+                    restored. All outstanding revenue will not be refunded.
+                </div>
+                <Button title="Close" onPress={close} />
+                <Button
+                    title="Cancel"
+                    onPress={navigateToParent}
+                    className="bg-transparent border-neutral-300"
+                />
+            </div>
+        </View>
+    );
 }
