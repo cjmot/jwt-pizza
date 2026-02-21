11e6c0a5945a938f9dc0bbb00a06f4782c483586
cjmot <146262493+cjmot@users.noreply.github.com>
2026-02-21 14:30:15 -0700

    update user interface to include editUser form

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     timeout: 5000,
     use: {
         baseURL: 'http://localhost:5173',
-        trace: 'retain-on-failure',
+        trace: 'on-first-retry',
     },
 
     /* Configure projects for major browsers */
