d61966cc9ba92034dc89c4bbe01629545963946a
cjmot <146262493+cjmot@users.noreply.github.com>
2026-02-21 17:37:33 -0700

    add deleteUser route to app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -18,6 +18,7 @@ import CreateStore from '../views/createStore';
 import CreateFranchise from '../views/createFranchise';
 import CloseFranchise from '../views/closeFranchise';
 import CloseStore from '../views/closeStore';
+import DeleteUser from '../views/deleteUser';
 import Payment from '../views/payment';
 import NotFound from '../views/notFound';
 import Docs from '../views/docs';
@@ -108,6 +109,12 @@ export default function App() {
             title: 'Close store',
             to: '/:subPath?/close-store',
             component: <CloseStore />,
+            display: [],
+        },
+        {
+            title: 'Delete user',
+            to: '/:subPath?/delete-user',
+            component: <DeleteUser />,
             display: [],
         },
         { title: 'Payment', to: '/payment', component: <Payment />, display: [] },
